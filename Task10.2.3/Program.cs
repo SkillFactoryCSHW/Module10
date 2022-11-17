e1dedbf38f7c498335bcc29be3df99681522143a
Bochanikita <bochanikita@yandex.ru>
2022-11-17 23:35:32 +0300

    Add task 10.2.2

---
+++ Task10.2.3/Program.cs
@@ -0,0 +1,11 @@
+﻿public class SomeClass : IWorker
+{
+    void IWorker.Build()
+    {
+        throw new NotImplementedException();
+    }
+}
+public interface IWorker
+{
+    public void Build();
+}
